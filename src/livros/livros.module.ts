9f0d611926fd4016ea548360c97d6ecffee80f9e
cosmeTeixeiraSilva <cosme.teixeira@gmail.com>
2025-09-10 11:07:34 -0300

    endpoint listar livros

--- src/livros/livros.module.ts
+++ src/livros/livros.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LivrosController } from './livros.controller';
 import { LivrosService } from './livros.service';
-
+import { PrismaModule } from 'src/prisma/prisma.module';
 @Module({
+  imports: [PrismaModule],
   controllers: [LivrosController],
-  providers: [LivrosService]
+  providers: [LivrosService],
 })
 export class LivrosModule {}
