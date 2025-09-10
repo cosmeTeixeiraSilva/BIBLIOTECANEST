cfcc0b0ef06ca10c900f5af654296f07d44a784d
cosmeTeixeiraSilva <cosme.teixeira@gmail.com>
2025-09-10 11:26:54 -0300

    feat:adicionando novas libs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+// main.ts
+//Importando o Método ValidationPipe
+import { ValidationPipe, BadRequestException } from "@nestjs/common";
+
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  //Validação Global
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove campos não definidos no DTO
+      forbidNonWhitelisted: true, // erro se vier campo extra
+      transform: true, // transforma tipos automaticamente
+      exceptionFactory: (errors) => {
+        // aqui você personaliza a resposta
+        const formattedErrors = errors.map((err) => ({
+          field: err.property,
+          errors: Object.values(err.constraints || {}),
+        }));
+
+        return new BadRequestException({
+          status: 400,
+          message: "Erro de validação nos campos",
+          errors: formattedErrors,
+        });
+      },
+    })
+  );
+
+  // Habilitando CORS
+  app.enableCors({
+    origin: "http://localhost:3000", // URL do frontend Next.js
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  });
+
+  await app.listen(3030);
+  console.log("Rodando o BackEnd (API) na porta: 3030");
 }
 bootstrap();
